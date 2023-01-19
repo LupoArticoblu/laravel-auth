f288e6498c8ba624890121a2dd911c6ca9eb4fad
LupoArticoblu <wolf94-@libero.it>
2023-01-19 09:37:10 +0100

    model post eliminato

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,5 +10,18 @@ class Portfolio extends Model
 {
     use HasFactory;
 
-    
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+        
+        $original_slug = $slug;
+        $c = 1;
+        $exists = Portfolio::where('slug', $slug)->first();
+        while($exists){
+            $slug = $original_slug . '-' . $c;
+            $exists = Portfolio::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+
+    }
 }
